fbedd29f52f81f2a59ac138906dba157e41e0df5
Arshiya <83arshiarezaie83@gmail.com>
2026-02-25 20:47:47 +0330

    feat(shell): wire router-outlet and verify feature page routing

--- client/src/app/features/shell/layout/app-layout/app-layout.component.ts
+++ client/src/app/features/shell/layout/app-layout/app-layout.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-app-layout',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.scss'
 })
