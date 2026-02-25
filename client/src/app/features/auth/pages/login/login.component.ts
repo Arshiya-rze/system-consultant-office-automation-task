39d3469816f4b3d1bb3ad0f956d177965208a285
Arshiya <83arshiarezaie83@gmail.com>
2026-02-25 21:33:30 +0330

    refactor(app): replace default template with router-outlet and refine login layout

--- client/src/app/features/auth/pages/login/login.component.ts
+++ client/src/app/features/auth/pages/login/login.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+   private fb = inject(FormBuilder);
+  private auth = inject(AuthService);
+  private router = inject(Router);
 
+  errorMessage = '';
+
+  form = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  submit(): void {
+    this.errorMessage = '';
+
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const username = this.form.value.username ?? '';
+    const password = this.form.value.password ?? '';
+
+    const ok = this.auth.login(username, password);
+
+    if (!ok) {
+      this.errorMessage = 'نام کاربری یا رمز عبور نامعتبر است.';
+      return;
+    }
+
+    this.router.navigateByUrl('/app');
+  }
 }
